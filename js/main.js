65315137ca47a1b9c32ddc0fcd5aebc03dad10cc
ahmed <ahmed.mohammed.developer@gmail.com>
2024-02-15 23:24:35 +0300

    End  gallery pagen

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
+let $links = $(".itemLinks");
+
+$links.click(function (e) {
+     $links.removeClass("active");
+     let ClickedLink = e.target;
+     ClickedLink = $(ClickedLink);
+     let position = ClickedLink.attr("data-pos");
+     let translateValue = "translateX(" + position * 25 + "%)";
+     $("#wrapper").css({
+          transform: translateValue
+     });
+
+     ClickedLink.addClass("active");
+});
+
+$($links[0]).addClass("active");
+
+
+
+
 let footer = document.getElementById("copyright");
 footer.innerHTML = new Date().getFullYear();
